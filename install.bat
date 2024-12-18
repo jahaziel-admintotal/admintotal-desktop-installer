370b28d02d6015861f3e70cdf0660abdff5dd4aa
Jahaziel Noriega <jahaziel@admintotal.com>
2024-12-18 11:39:39 -0700

    hf

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ REM Cambia al directorio donde se encuentra el script BAT
 cd /d "%~dp0"
 
 echo Descargando las últimas versiones de las imágenes de los contenedores...
-docker compose pull
+docker compose up
 
 echo Descarga completada. Puedes iniciar los contenedores desde Docker Desktop.
 pause
